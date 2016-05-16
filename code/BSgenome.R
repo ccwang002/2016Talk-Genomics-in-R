11394e93dd51be5f6af263684998323f3aa50f2d
Liang-Bo Wang <me@liang2.tw>
2016-05-16 23:33:10 +0800

    Update BSgenome code and slides content

--- code/BSgenome.R
+++ code/BSgenome.R
@@ -1,6 +1,3 @@
-library(BSgenome)
-head(available.genomes())
-
 library(BSgenome.Hsapiens.UCSC.hg19)
 Hsapiens
 
@@ -9,10 +6,23 @@ head(chrom_names)
 
 getSeq(Hsapiens, chrom_names[1:2])
 
-
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 txs <- transcriptsBy(txdb, by="gene")
 txs[["5594"]]
+exby = exonsBy(txdb, by = "tx", use.names = TRUE)
 
-getSeq(Hsapiens, txby[["5594"]])
+getSeq(Hsapiens, txs[["5594"]])
 getSeq(Hsapiens, exby[["uc002zvn.3"]])
+
+library(Homo.sapiens)
+human <- Homo.sapiens
+
+# Full transcript sequence including UTRs, exons, and introns
+UCSC_MAPK1_ext <- readDNAStringSet("UCSC_Genome_Browser_MAPK1.fasta")
+UCSC_MAPK1_ext == getSeq(Hsapiens, txs[["5594"]])[[1]]
+
+# Transcript sequence including UTRs and exons
+UCSC_MAPK1_exon_ext <- readDNAStringSet("UCSC_Genome_Browser_MAPK1_exon.fasta")
+merged_exon_seq <- unlist(getSeq(Hsapiens, exby[["uc002zvn.3"]]))
+UCSC_MAPK1_exon_ext == merged_exon_seq
